7155f076a7bbd9325bbde83bf2038ad7bef62a39
tsmmythBR <47115775+tsmmythBR@users.noreply.github.com>
2019-01-29 19:23:57 +0100

    Update video.py

--- video.py
+++ video.py
@@ -14,17 +14,27 @@ Clientdiscord = discord.Client()
 
 @client.event
 async def on_ready():
-    await client.change_presence(game=Game(name='SnorWare V0.22', type = 2))
+    await client.change_presence(game=Game(name='SnorWare V0.3', type = 2))
     print('snorbot is ready :)')
 
 
 @client.event
 async def on_message(message):
+    if message.content == 'help':
+        em = discord.Embed(description='Commands: !yesno, !coinflip, !friend, !8ball, ever1')
+        em.set_image(url='https://discordapp.com/assets/ba74954dde74ff40a32ff58069e78c36.png')
+        await client.send_message(message.channel, embed=em)
+    if message.content.startswith('!yesno'):
+        randomlist = ['Yes','No'']
+        await client.send_message(message.channel,(random.choice(randomlist)))
+    if message.content.startswith('!coinflip'):
+        randomlist = ['head','coin'']
+        await client.send_message(message.channel,(random.choice(randomlist)))
     if message.content.startswith('!friend'):
         randomlist = ['10%','20%','30%','40%','50%','60%','70%','80%','90%','100%']
         await client.send_message(message.channel,(random.choice(randomlist)))
     if message.content.startswith('!8ball'):
-        randomlist = ['Ja','Nee','Zeker','Nope','wtf','WAHAHA NOPE']
+        randomlist = ['Yes','No','sure','Nope','wtf','WAHAHA NOPE']
         await client.send_message(message.channel,(random.choice(randomlist)))
     if message.content == 'snor':
         em = discord.Embed(description='hier heb je een foto van een snor :)')
@@ -38,22 +48,16 @@ async def on_message(message):
         em = discord.Embed(description='hier heb je een foto van een skipper...')
         em.set_image(url='https://media.discordapp.net/attachments/491228753268310018/539507547569258496/865db2d7-cddc-4ab2-8f29-e41fe87b74d3-1526542591593.png?width=968&height=403')
         await client.send_message(message.channel, embed=em)
+    if message.content == 'pls 4k':
+        em = discord.Embed(description='Je hebt geen toegang tot deze command. De fbi is onderweg...')
+        em.set_image(url='https://memegenerator.net/img/images/72477136.jpg')
+        await client.send_message(message.channel, embed=em)
+    if message.content == 'Pls 4k':
+        em = discord.Embed(description='Je hebt geen toegang tot deze command. De fbi is onderweg...')
+        em.set_image(url='https://memegenerator.net/img/images/72477136.jpg')
+        await client.send_message(message.channel, embed=em)
     if message.content == 'ever1':
         await client.send_message(message.channel,'@everyone 1')
-    if message.content == 'gozer':
-        await client.send_message(message.channel,'G*st, Stop met het g woord. Demanmetsnor112 wordt er bang van!')
-    if message.content == 'Gozer':
-        await client.send_message(message.channel,'G*st, Stop met het g woord. Demanmetsnor112 wordt er bang van!')
-    if message.content == '@Matthijs0409':
-        await client.send_message(message.channel,'vieze nazi, ga snor niet taggen')
-    if ('gozer') in message.content:
-       await client.delete_message(message)
-    if ('Gozer') in message.content:
-       await client.delete_message(message)
-    if ('Gast') in message.content:
-       await client.delete_message(message)
-    if ('gast') in message.content:
-       await client.delete_message(message)
     if ('ever1') in message.content:
        await client.delete_message(message)
     if ('@everyone 1') in message.content:
