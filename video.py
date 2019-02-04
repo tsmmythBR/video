3f01532694f1410023da9fa0edac56e9fc446104
tsmmythBR <47115775+tsmmythBR@users.noreply.github.com>
2019-02-04 09:49:27 +0100

    Update video.py

--- video.py
+++ video.py
@@ -63,8 +63,7 @@ async def on_message(message):
     if ('@everyone 1') in message.content:
        await client.delete_message(message)
     if message.content == 'ever1 matt':
-        await client.send_message(message.channel,'@Matthijs0409ツ
-#7418')
+        await client.send_message(message.channel,'@Matthijs0409ツ')
     if ('ever1 matt') in message.content:
        await client.delete_message(message)                               
 client.run('NTM5NTA1NDgwMTk2OTQ3OTg4.DzDVoQ.5qLPGFkyNJQb4mJSqz-TEAy1h_0')
