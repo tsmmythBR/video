6e3c11d6d25863cb4959bd7b6a9494ee5f75b836
tsmmythBR <47115775+tsmmythBR@users.noreply.github.com>
2019-02-04 09:57:32 +0100

    Update video.py

--- video.py
+++ video.py
@@ -45,7 +45,7 @@ async def on_message(message):
         em.set_image(url='https://cdn.discordapp.com/attachments/491228753268310018/539507413766635521/schaap.jpg')
         await client.send_message(message.channel, embed=em)
     if message.content == 'yannick':
-        em = discord.Embed(description='hier heb je een foto van een skipper...')
+        em = discord.Embed(description='hier heb je een foto van een skipper... hij is erg gay')
         em.set_image(url='https://media.discordapp.net/attachments/491228753268310018/539507547569258496/865db2d7-cddc-4ab2-8f29-e41fe87b74d3-1526542591593.png?width=968&height=403')
         await client.send_message(message.channel, embed=em)
     if message.content == 'pls 4k':
@@ -63,7 +63,15 @@ async def on_message(message):
     if ('@everyone 1') in message.content:
        await client.delete_message(message)
     if message.content == 'ever1 matt':
-        await client.send_message(message.channel,'@541903920310648843')
+        await client.send_message(message.channel,'Deze command is buiten gebruik.')
+    if message.content == 'ever1 storm':
+        await client.send_message(message.channel,'Deze command is buiten gebruik.')
+    if message.content == 'ever1 yan':
+        await client.send_message(message.channel,'Deze command is buiten gebruik.')
     if ('ever1 matt') in message.content:
-       await client.delete_message(message)                               
+       await client.delete_message(message)     
+    if ('ever1 storm') in message.content:
+       await client.delete_message(message)  
+    if ('ever1 yan') in message.content:
+       await client.delete_message(message)  
 client.run('NTM5NTA1NDgwMTk2OTQ3OTg4.DzDVoQ.5qLPGFkyNJQb4mJSqz-TEAy1h_0')
